40e69fdad664199b5de1915d61508eba1c1a7fdf
wongaokay <23andrewwong@gmail.com>
2023-11-20 04:05:55 -0800

    initial commit

---
+++ example.py
@@ -0,0 +1,37 @@
+import zmq
+
+
+def update_expense(cat_to_update, amount_to_subtract):
+    """Both request and receives microservice"""
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5555")
+
+    # Request Microservice
+    # Assign current_expense to variable given cat_to_update
+    current_expense = category_dict[cat_to_update]
+
+    socket.send_string("User requesting to update category")
+    if socket.recv_string() == "REQUEST_DATA":
+        data = {"current_expense": current_expense, "amount_to_subtract": amount_to_subtract}
+        socket.send_json(data)
+
+    # Receive Microservice
+    updated_expense = int(socket.recv_string())
+
+    # Update dictionary
+    category_dict[cat_to_update] = updated_expense
+
+    print(f"Subtracted ${amount_to_subtract} from {current_expense}")
+    print(f"{cat_to_update}: {category_dict[cat_to_update]}")
+
+
+category_dict = {
+    'Groceries': 195,
+    'Rent': 1200
+}
+
+
+if __name__ == "__main__":
+    for request in range(1):
+        update_expense("Rent", 50)
---
+++ update_expense.py
@@ -0,0 +1,32 @@
+import time
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+while True:
+    # Receives request from client
+    received_request = socket.recv().decode('utf-8')
+    print(received_request)
+
+    time.sleep(.5)
+
+    # Requests user data from client {"current_expense": ?, "amount_to_subtract: ?"}
+    print("Requesting data from client")
+    socket.send_string("REQUEST_DATA")
+    data = socket.recv_json()
+
+    current_expense = data["current_expense"]
+    amount_to_subtract = data["amount_to_subtract"]
+
+    # Calculate Updated Amount
+    updated_expense = current_expense - amount_to_subtract
+    # Prevent negative expense amounts
+    if updated_expense < 0:
+        updated_expense = 0
+
+    time.sleep(.5)
+
+    print(f"Updated expense: ${updated_expense}")
+    socket.send_string(str(updated_expense))
